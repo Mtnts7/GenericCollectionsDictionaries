daccc9365aa0853bb6138baea1a8188955b8596d
Mehmet Metintaş <mehmet.metintas@outlook.com>
2021-01-26 01:42:53 +0300

    Add project files.

---
+++ GenericCollectionsDictionaries/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionsDictionaries
+{
+    class MyDictionary<Tkey,TValue>
+    {
+        Tkey[] _keyArray;
+        TValue[] _valueArray;
+
+        Tkey[] _tempKeyArray;
+        TValue[] _tempValueArray;
+
+        public MyDictionary()
+        {
+            _keyArray = new Tkey[0];
+            _valueArray = new TValue[0];
+        }
+        
+        public void Add(Tkey key, TValue value) 
+        {
+            _tempKeyArray = _keyArray;
+            _tempValueArray = _valueArray;
+
+            _keyArray = new Tkey[_keyArray.Length + 1];
+            _valueArray = new TValue[_valueArray.Length + 1];
+
+            for (int i = 0; i < _tempKeyArray.Length; i++)
+            {
+                _keyArray[i] = _tempKeyArray[i];
+            }
+
+            for (int i = 0; i < _tempKeyArray.Length; i++)
+            {
+                _valueArray[i] = _tempValueArray[i];
+            }
+            _keyArray[_keyArray.Length - 1]=key;
+            _valueArray[_valueArray.Length - 1] = value;
+
+        }
+        public Tkey[] Mam1
+        {
+            get { return _keyArray; }
+
+        }
+        public TValue[] Mam2
+        {
+            get { return _valueArray; }
+
+        }
+
+
+
+    }
+}
---
+++ GenericCollectionsDictionaries/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollectionsDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> plakalar = new MyDictionary<int, string>();
+            plakalar.Add(35, "İzmir");
+            plakalar.Add(41, "Kocaeli");
+            plakalar.Add(06, "Ankara");
+            plakalar.Add(34, "İstanbul");
+
+            Console.WriteLine(plakalar.Mam1[0] + ":" + plakalar.Mam2[0]);
+            Console.WriteLine(plakalar.Mam1[1] + ":" + plakalar.Mam2[1]);
+            Console.WriteLine(plakalar.Mam1[2] + ":" + plakalar.Mam2[2]);
+            Console.WriteLine(plakalar.Mam1[3] + ":" + plakalar.Mam2[3]);
+        }
+        
+    }
+    
+}
